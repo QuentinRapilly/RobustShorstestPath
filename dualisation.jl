b34a3fc8b1fe01df0938c5ef31c9201465dd3ee0
yohan.bornachot <yohan.bornachot@gmail.com>
2022-01-25 14:03:07 +0100

    [*] minor changes (julia parsing errors corrected)

--- dualisation.jl
+++ dualisation.jl
@@ -18,23 +18,19 @@ function dualisation(n :: Int, s::Int, t::Int, p::Array{Int,1},
     @variable(m, alpha_2_0 >= 0)
 
     ### Objective
-    @objective(m, Min, sum(x[r[1],r[2]]*r[3] + alpha_1[r[1],[2]]*r[4] for r in Mat) + alpha_1_0*d1)
+    @objective(m, Min, sum(x[r[1]][r[2]]*r[3] + alpha_1[r[1]][r[2]]*r[4] for r in Mat) + alpha_1_0*d1)
 
     ### Constraints
-    @constraint(m, [r in Mat], alpha_1[r[1],r[2]] + alpha_1_0 >= r[3]*x[r[1],r[2]]])
+    @constraint(m, [r in Mat], alpha_1[r[1]][r[2]] + alpha_1_0 >= r[3]*x[r[1]][r[2]])
     @constraint(m, [i in 1:n], alpha_2[i] + alpha_2_0 >= p_hat[i]*y[i])
     @constraint(m, sum(p[i]*y[i]+2*alpha_2[i] for i in 1:n) + alpha_2_0*d2 <= S)
     
     @constraint(m, y[s]==1)
     @constraint(m, y[t]==1)
 
-
     @constraint(m, [i in 1:n; i!=s], sum(x[j,i]*exist_road[j,i] for j in 1:n) == y[i])
     @constraint(m, [i in 1:n; i!=t], sum(x[i,j]*exist_road[i,j] for j in 1:n) == y[i])
     
     # Optimize the problem
     optimize!(m)
-
-
-
 end
